ab20f28a341d27b0dd8509b3cd7571e5742cf732
yuhong <153108317@qq.com>
2017-04-17 17:48:22 +0800

    RecylerViewAdapter 添加刷新事件

--- app/src/main/java/com/example/yh/myapplication/fragments/PictureFragment1.java
+++ app/src/main/java/com/example/yh/myapplication/fragments/PictureFragment1.java
@@ -2,6 +2,7 @@ package com.example.yh.myapplication.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import com.example.yh.myapplication.base.BasicFragment;
 import com.example.yh.myapplication.entities.PictureBean;
 import com.example.yh.myapplication.network.HttpUrls;
 import com.example.yh.myapplication.result.PictureResult;
+import com.example.yh.myapplication.utils.Log;
 import com.example.yh.myapplication.views.IView;
 
 import java.util.ArrayList;
@@ -30,8 +32,10 @@ import butterknife.ButterKnife;
  * Copyright (c) 2016 d2cmall. All rights reserved.
  */
 
-public class PictureFragment1 extends BasicFragment implements IView<PictureResult>{
+public class PictureFragment1 extends BasicFragment implements IView<PictureResult>,SwipeRefreshLayout.OnRefreshListener{
     @BindView(R.id.mrecylerview) RecyclerView mRecyclerView;
+    @BindView(R.id.swiperefreshLayout)
+    SwipeRefreshLayout mSwiperefreshLayout;
     private Picture1RecylerViewAdapter mPicture1RecylerViewAdapter;
     private List<PictureBean> mList;
     private int orientation=LinearLayoutManager.HORIZONTAL;
@@ -47,6 +51,7 @@ public class PictureFragment1 extends BasicFragment implements IView<PictureResu
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mSwiperefreshLayout.setOnRefreshListener(this);
         mList=new ArrayList<>();
         mPicture1RecylerViewAdapter=new Picture1RecylerViewAdapter(mList,R.layout.reycelerview_picture1_item);
         LinearLayoutManager manager=new LinearLayoutManager(getActivity());
@@ -63,5 +68,23 @@ public class PictureFragment1 extends BasicFragment implements IView<PictureResu
             mPicture1RecylerViewAdapter.notifyDataSetChanged();
           //  mPicture1RecylerViewAdapter.notify();
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.e("onrefresh");
+        new Thread(
+        )
+        {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                   sleep(3000l);
+                    mSwiperefreshLayout.setRefreshing(false);
+                }catch (Exception e){}
+
+            }
+        }.start();
     }
 }
