cfcd5d9112bb6710f124808eaa97203a4ed201ff
yuhong <153108317@qq.com>
2017-04-18 14:53:37 +0800

    在basicActivity 中添加无数据覆盖页面

--- app/src/main/java/com/example/yh/myapplication/base/BasicActivity.java
+++ app/src/main/java/com/example/yh/myapplication/base/BasicActivity.java
@@ -7,6 +7,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.yh.myapplication.R;
 
 import java.util.List;
 
@@ -14,15 +20,35 @@ import java.util.List;
  * Created by yh on 2017/3/29.
  */
 
-public class BasicActivity extends FragmentActivity {
+public class BasicActivity extends FragmentActivity implements View.OnClickListener {
     protected List<BasicFragment> listFragments;
     public BasicActivity mBasicActivity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        super.setContentView(R.layout.activity_basic);
         mBasicActivity = this;
+    }
+
+    public ViewStub stubneterror;
+    private ImageView mImageback;
+    private ImageView mImageviewmsg;
+
+    public void setContentView(int layoutId) {
+        ViewStub stubtitle = (ViewStub) findViewById(R.id.viewstub_titlebar);
+        ViewStub stubcontent = (ViewStub) findViewById(R.id.viewstub_content);
+
+        stubtitle.setLayoutResource(R.layout.titlebar);
+        stubtitle.inflate();
+        stubcontent.setLayoutResource(layoutId);
+        mImageback = (ImageView) findViewById(R.id.imageview_back);
+        mImageviewmsg = (ImageView) findViewById(R.id.imageview_msg);
+        mImageback.setOnClickListener(this);
+        mImageviewmsg.setOnClickListener(this);
+        stubcontent.inflate();
+
+
     }
 
     public void openActivity(Class<? extends Activity> activity, Bundle bundle) {
@@ -48,5 +74,30 @@ public class BasicActivity extends FragmentActivity {
         }
 
         ft.commit();
+    }
+
+    private LinearLayout mLinearLayoutNetError;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageview_back:
+                finish();
+                break;
+            case R.id.imageview_msg:
+                if (stubneterror == null) {
+                    stubneterror = (ViewStub) findViewById(R.id.viewstub_net_error);
+                    mLinearLayoutNetError = (LinearLayout) stubneterror.inflate();
+
+                    mLinearLayoutNetError.setOnClickListener(this);
+                } else {
+                    stubneterror.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.net_errorId:
+                mLinearLayoutNetError.setVisibility(View.GONE);
+                break;
+
+        }
     }
 }
